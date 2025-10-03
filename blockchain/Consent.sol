5fa3af586738e3a50f4dbf861aa8c2b9a133f535
Siri Madhuri <126378302+sirimadhuri02@users.noreply.github.com>
2025-10-03 23:52:45 +0530

    Update Consent.sol

--- blockchain/Consent.sol
+++ blockchain/Consent.sol
@@ -1,1 +1,16 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Consent {
+    struct ConsentRecord {
+        string userId;
+        string timestamp;
+    }
+
+    mapping(string => ConsentRecord) public consents;
+
+    function storeConsent(string memory userId, string memory timestamp) public {
+        consents[userId] = ConsentRecord(userId, timestamp);
+    }
+}
